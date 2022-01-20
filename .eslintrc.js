1a1db352fbd85170b72e51adc916fc1eada9659d
Professor Hex <HexBlit@users.noreply.github.com>
2022-01-19 18:11:17 -0700

    Remove prettier\prettier Delete 'CR' Error
    Resolve Prettier error of 'cr' that appeared when running on a windows machine.

--- .eslintrc.js
+++ .eslintrc.js
@@ -78,6 +78,11 @@ module.exports = {
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
-    "prettier/prettier": "error"
+    "prettier/prettier": [
+      "error",
+      {
+        'endOfLine': 'auto',
+      }
+    ]
   }
 }
