6d53c6ab3511287823f9668682efdb7915fe1a0c
DonaldPeat <don@caleos.io>
2022-01-20 17:12:38 -0600

    refactor:  remove unrealated from main layout & placeholder for producer map

---
+++ src/components/ProducerMap.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.producer-map 
+  .producer-map-center Producer Map Here
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ProducerMap',
+  props: {}
+});
+</script>
+<style lang="sass">
+.producer-map
+  height: 20rem
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,105 +1,35 @@
 <template lang="pug">
 q-layout( view="lHh Lpr lFf" )
   q-header( elevated )
-    q-toolbar
-      q-btn(
+    q-btn(
         flat
         dense
         round
         icon="menu"
         aria-label="Menu"
-        @click="toggleLeftDrawer"
-      )
-      q-toolbar-title Quasar App
-
-      div Quasar v{{ $q.version }}
-
-  q-drawer(
-    v-model="leftDrawerOpen"
-    show-if-above
-    bordered
-  )
-    q-list
-      q-item-label( header ) Essential Links
-
-      EssentialLink(
-        v-for="link in essentialLinks"
-        :key="link.title"
-        v-bind="link"
-      )
-
+        @click=""
+    )
+      q-toolbar-title Header OBE v0.0.0
+  ProducerMap
   q-page-container
     router-view 
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue';
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
+import ProducerMap from 'components/ProducerMap.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
+    ProducerMap
   },
 
   data() {
-    return {
-      leftDrawerOpen: false,
-      essentialLinks: linksList
-    };
+    return {};
   },
 
-  methods: {
-    toggleLeftDrawer() {
-      this.leftDrawerOpen = !this.leftDrawerOpen;
-    }
-  }
+  methods: {}
 });
 </script>
