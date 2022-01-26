0e4f2aa14f7448177a7e2a7b8d13c850c7a108b1
mr_ceh <webmaster.webdeveloper@gmail.com>
2022-01-26 08:17:01 -0800

    add Network, Vote, Proposal, Explore, THERMS, PRIVACY, REPOSITORY, path

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,6 +7,48 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/Index.vue') }]
   },
+  {
+    path: '/network',
+    name: 'Network',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Index.vue') }]
+  },
+  {
+    path: '/vote',
+    name: 'Vote',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Index.vue') }]
+  },
+  {
+    path: '/proposal',
+    name: 'Proposal',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Index.vue') }]
+  },
+  {
+    path: '/explore',
+    name: 'Explore',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Index.vue') }]
+  },
+  {
+    path: '/therms',
+    name: 'THERMS',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Index.vue') }]
+  },
+  {
+    path: '/privacy',
+    name: 'PRIVACY',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Index.vue') }]
+  },
+  {
+    path: '/explore',
+    name: 'REPOSITORY',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Index.vue') }]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
