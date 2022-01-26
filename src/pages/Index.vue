403b9501911b3f3c0d1a048a449d584aa1f35bb4
mr_ceh <webmaster.webdeveloper@gmail.com>
2022-01-26 12:02:57 -0800

    Historical Price Chart Component added to index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,16 @@
 <template lang="pug">
 q-page.row.items-center.justify-evenly
+  PriceChart
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import PriceChart from 'components/PriceChart.vue';
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+    PriceChart
+  }
 });
 </script>
