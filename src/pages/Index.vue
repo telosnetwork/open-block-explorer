bea97233f36b000632d4f11afea2e0bc9171f747
mr_ceh <webmaster.webdeveloper@gmail.com>
2022-01-27 04:54:27 -0800

    remove price chart and move it into network page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,12 @@
 <template lang="pug">
 q-page.row.items-center.justify-evenly
-  PriceChart
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import PriceChart from 'components/PriceChart.vue';
 
 export default defineComponent({
   name: 'PageIndex',
-  components: {
-    PriceChart
-  }
+  components: {}
 });
 </script>
