a378f29ee56cea5a17e9609db5b8b4e6d13aa0eb
DonaldPeat <don@caleos.io>
2022-01-28 04:26:15 -0600

    fix: uncomment  hyperion boot

--- src/boot/hyperion.ts
+++ src/boot/hyperion.ts
@@ -1,14 +1,14 @@
-// import { boot } from 'quasar/wrappers';
-// import axios, { AxiosInstance } from 'axios';
+import { boot } from 'quasar/wrappers';
+import axios, { AxiosInstance } from 'axios';
 
-// declare module '@vue/runtime-core' {
-//   interface ComponentCustomProperties {
-//     $hyperion: AxiosInstance;
-//   }
-// }
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $hyperion: AxiosInstance;
+  }
+}
 
-// const hyperion = axios.create({ baseURL: process.env.HYPERION_ENDPOINT });
+const hyperion = axios.create({ baseURL: process.env.HYPERION_ENDPOINT });
 
-// export default boot(({ app }) => {
-//   app.config.globalProperties.$hyperion = hyperion;
-// });
+export default boot(({ app }) => {
+  app.config.globalProperties.$hyperion = hyperion;
+});
