9dfc6202bfebaba760c8c0ba9b6669b6a7dd418b
DonaldPeat <don@caleos.io>
2022-02-09 06:01:37 -0600

    refactor: remove hyperion from global interface

--- src/types/vue-global.d.ts
+++ src/types/vue-global.d.ts
@@ -3,7 +3,6 @@ import { APIClient } from '@greymass/eosio';
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $hyperion: AxiosInstance;
     $axios: AxiosInstance;
     $api: APIClient;
   }
