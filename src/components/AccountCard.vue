03e9875bdf1fbf2e0f4675b1f8781ad08c060ad4
Don Peat <don@caleos.io>
2022-02-23 04:52:51 -0600

    feat: view resources for account" "

--- src/components/AccountCard.vue
+++ src/components/AccountCard.vue
@@ -2,20 +2,25 @@
 .q-pa-md
   q-card.account-card
     q-card-section
-      .text-h6 {{ account }}
-      .text-subtitle2 created by 
-        router-link( :to="{ path: 'account', params: { account: creatingAccount}}") {{ creatingAccount }}
+      .inline-section
+        .text-h6 {{ account }}
+        .text-subtitle2 created by 
+          router-link( :to="{ path: 'account', params: { account: creatingAccount}}") {{ creatingAccount }}
+      .resources.inline-section
+        .resource.inline-section CPU {{ cpu }}% used
+        .resource.inline-section NET {{ net }}% used
+        .resource.inline-section RAM {{ ram }}% used
     q-markup-table
       thead
         tr
           th.text-left BALANCE
-        tbody
+        tbody.table-body
           tr
             td.text-left TOTAL
             td.text-right {{ total }}
           tr
             td.text-left REFUNDING
-            td.text-right {{ refunding }}
+            td.text-right {{ refunding }} 
           tr
             td.text-left STAKED BY OTHERS
             td.text-right {{ staked }}
@@ -24,23 +29,74 @@
             td.text-right {{ rex }}
 </template>
 <script lang="ts">
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import { defineComponent } from 'vue';
+
+const HUNDRED = 100.0;
 export default defineComponent({
   name: 'AccountCard',
+  props: {
+    account: {
+      type: String,
+      required: true
+    }
+  },
   data() {
     return {
-      account: 'john',
-      creatingAccount: 'bob',
-      total: 9999,
-      refunding: 3333,
-      staked: 2222,
-      rex: 4444
+      creatingAccount: '',
+      total: '',
+      refunding: '',
+      staked: '',
+      rex: '',
+      ram: '',
+      cpu: '',
+      net: ''
     };
+  },
+  async mounted() {
+    await this.loadAccountData();
+  },
+  methods: {
+    async loadAccountData(): Promise<void> {
+      const data = await this.$api.getAccount(this.account);
+      this.total = data.account.core_liquid_balance;
+      this.refunding = data.account.refund_request
+        ? data.account.refund_request
+        : '0 TLOS';
+      this.staked = data.account.voter_info.staked
+        ? data.account.voter_info.staked
+        : '0 TLOS';
+      this.rex = data.account.rex_info ? data.account.rex_info : '0 TLOS';
+      this.ram = (
+        (data.account.ram_usage / data.account.total_resources.ram_bytes) *
+        HUNDRED
+      ).toFixed(2);
+      this.cpu = (
+        (data.account.cpu_limit.used / data.account.cpu_limit.max) *
+        HUNDRED
+      ).toFixed(2);
+      this.net = (
+        (data.account.net_limit.used / data.account.net_limit.max) *
+        HUNDRED
+      ).toFixed(2);
+    }
   }
 });
 </script>
 <style lang="sass" scoped>
 .account-card
+  max-width: 100%
+.table-body
   width: 100%
-  max-width: 500px
+  display: table
+.inline-section
+  display: inline-block
+.resources
+  float: right
+  margin-top: 2.5rem
+  margin-right: 3rem
+.resource
+  margin-right: 2rem
 </style>
