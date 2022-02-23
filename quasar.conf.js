bb689322114a193ee978474a6d115af4c870f6cb
Don Peat <don@caleos.io>
2022-02-23 00:13:12 -0600

    fix: uncomment public path

--- quasar.conf.js
+++ quasar.conf.js
@@ -53,7 +53,7 @@ module.exports = configure(function (ctx) {
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
-      // publicPath: '/',
+      publicPath: '/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
