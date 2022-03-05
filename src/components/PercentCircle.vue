d7e2479f887564826112e2f43a995ccce22d7217
Don Peat <don@caleos.io>
2022-03-04 23:29:08 -0600

    refactor: rework radial chart

--- src/components/PercentCircle.vue
+++ src/components/PercentCircle.vue
@@ -1,32 +1,20 @@
 <template lang="pug">
-svg.circular-chart(:style="{ 'max-width': containerWidth }" :viewBox="`0 ${-space / 2} ${containerWidth} ${containerWidth}`" )
-  path.circle.data-circle(
+svg.circular-chart(:style="{ 'max-width': containerWidth }" :viewBox="`${-offset * 6} ${-offset / 2} ${containerWidth} ${containerWidth}`" )
+  path.circle-bg( 
+    :d="`M18 2 a ${radius} ${radius} 0 0 1 0 88 a ${radius} ${radius} 0 0 1 0 ${-diameter}`"
+    )
+  path.circle( 
     :stroke-dasharray="dashArray"
-    d="M46 2 a 44 44 0 0 1 0 88 a 44 44 0 0 1 0 -88"
-    fill="none"
-    :stroke="strokeColor"
+    :d="`M18 2 a ${radius} ${radius} 0 0 1 0 88 a ${radius} ${radius} 0 0 1 0 ${-diameter}`"
+    :stroke='strokeColor'
     )
-  path.circle(
-    :stroke-dasharray="`${circumference}, ${circumference}`"
-    d="M46 2 a 44 44 0 0 1 0 88 a 44 44 0 0 1 0 -88"
-    fill="none"
-    stroke="white"
-    stroke-opacity=".13"
-    stroke-width="1"
-    )
-  text( :x="radius" y="38"
-    text-anchor="middle"
-    dominant-baseline="middle"
-    fill="white"
-    font-size="14px"
-    font-weight=700
+  text.text.label( 
+    x="18"
+    :y="radius - offset"
     ) {{ label }}
-  text( :x="radius + 3" y="56" 
-    text-anchor="middle"
-    dominant-baseline="middle"
-    fill="white"
-    fill-opacity=".5"
-    font-size="14px"
+  text.text.percentage( 
+    x="20" 
+    :y="percentOffsetY" 
     ) {{ percentage }}%
 </template>
 <script lang="ts">
@@ -39,20 +27,20 @@ export default defineComponent({
   props: {
     percentage: {
       type: Number,
-      required: true
+      default: 0
     },
     label: {
       type: String,
-      required: true
+      default: ''
     },
     radius: {
       type: Number,
-      required: true
+      default: 0
     }
   },
   data() {
     return {
-      space: 10
+      offset: 5
     };
   },
   computed: {
@@ -60,10 +48,10 @@ export default defineComponent({
       return 2 * this.radius;
     },
     containerWidth(): number {
-      return this.diameter + this.space;
+      return this.diameter + 2 * this.offset;
     },
     strokeColor(): string {
-      return this.percentage < 90 ? 'white' : 'red';
+      return this.percentage >= 90 ? 'red' : 'white';
     },
     circumference(): number {
       return 2 * PI * this.radius;
@@ -71,6 +59,9 @@ export default defineComponent({
     dashArray(): string {
       const scaledPath = (this.percentage / 100) * this.circumference;
       return `${scaledPath}, ${this.circumference}`;
+    },
+    percentOffsetY(): number {
+      return this.radius + 14;
     }
   }
 });
@@ -80,16 +71,30 @@ export default defineComponent({
   display: inline-block
   margin: 0 6px
 
+.circle-bg
+  fill: none
+  stroke: #eee
+  stroke-width: 2
+  stroke-opacity: .5
+
 .circle
   fill: none
-  stroke-width: 2.8
+  stroke-width: 5
   stroke-linecap: round
-
-.data-circle
-  stroke-width: 5px
-  animation: progress 1s ease-out forwards
 
 @keyframes progress
   0%
     stroke-dasharray: 0 100
+
+.text
+  text-anchor: middle
+  dominant-baseline: middle
+  fill: white
+  font-size: 14px
+
+.label
+  font-weight: 700
+
+.percentage
+  fill-opacity: .5
 </style>
