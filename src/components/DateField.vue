a576213e9859bfb86048604331e415acaf56c02a
Don Peat <don@caleos.io>
2022-03-06 18:24:12 -0600

    feat: add date field component
    chore: install moment

---
+++ src/components/DateField.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  div {{ friendlyDate }}
+</template>
+
+<script>
+import * as moment from 'moment';
+
+export default {
+  name: 'DateField',
+  props: {
+    epoch: {
+      type: Number,
+      required: true
+    },
+    showAge: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    friendlyDate() {
+      if (this.showAge) {
+        return moment.unix(this.epoch).fromNow();
+      }
+      return `${moment.unix(this.epoch).format('YYYY-MM-DD h:mm:ss')}`;
+    }
+  }
+};
+</script>
+
+<style scoped></style>
