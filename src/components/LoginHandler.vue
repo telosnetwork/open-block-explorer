51cdc276d2a7cfc50f4956c230d1b784cdf43f9c
Don Peat <don@caleos.io>
2022-03-17 03:29:32 -0500

    feat: user dropdown

--- src/components/LoginHandler.vue
+++ src/components/LoginHandler.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { mapActions, mapGetters, mapMutations } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 import LoginHandlerDropdown from './LoginHandlerDropdown.vue';
 import WalletModal from './WalletModal.vue';
 
@@ -23,36 +23,7 @@ export default defineComponent({
     ...mapGetters({ account: 'account/accountName' })
   },
   methods: {
-    ...mapMutations({ setAccountName: 'account/setAccountName' }),
-    ...mapActions({ logout: 'account/logout' }),
-    getAuthenticator() {
-      const wallet = localStorage.getItem('autoLogin');
-      const availAuthenticators =
-        this.$ual.getAuthenticators().availableAuthenticators;
-      const idx = availAuthenticators.findIndex(
-        (auth) => auth.constructor.name === wallet
-      );
-      return availAuthenticators[idx];
-    },
-    async onLogout(): Promise<void> {
-      const authenticator = this.getAuthenticator();
-      try {
-        authenticator && (await authenticator.logout());
-      } catch (error) {
-        console.log('Authenticator logout error', error);
-      }
-      this.setAccountName('');
-      this.clearLocalStorage();
-      if (this.$route.path !== '/') {
-        await this.$router.push({ path: '/' });
-      }
-    },
-    clearLocalStorage(): void {
-      localStorage.removeItem('autoLogin');
-      localStorage.removeItem('account');
-      localStorage.removeItem('anchor-link--list');
-      localStorage.removeItem('autoLogin');
-    }
+    ...mapMutations({ setAccountName: 'account/setAccountName' })
   }
 });
 </script>
--- src/components/LoginHandlerDropdown.vue
+++ src/components/LoginHandlerDropdown.vue
@@ -1,15 +1,72 @@
 <script lang="ts">
+// import { User } from 'universal-authenticator-library';
 import { defineComponent } from 'vue';
+import { mapActions, mapMutations } from 'vuex';
 export default defineComponent({
   name: 'LoginHandlerDropdown',
-  props: ['account']
+  props: ['account'],
+  data() {
+    return {
+      accounts: [this.account]
+    };
+  },
+  computed: {
+    disconnectLabel(): string {
+      return this.accounts.length > 1 ? 'Disconnect all' : 'Disconnect';
+    }
+  },
+  methods: {
+    ...mapMutations({ setAccountName: 'account/setAccountName' }),
+    ...mapActions({ logout: 'account/logout' }),
+    getAuthenticator() {
+      const wallet = localStorage.getItem('autoLogin');
+      const availAuthenticators =
+        this.$ual.getAuthenticators().availableAuthenticators;
+      const idx = availAuthenticators.findIndex(
+        (auth) => auth.constructor.name === wallet
+      );
+      return availAuthenticators[idx];
+    },
+    async onLogout(): Promise<void> {
+      const authenticator = this.getAuthenticator();
+      try {
+        authenticator && (await authenticator.logout());
+      } catch (error) {
+        console.log('Authenticator logout error', error);
+      }
+      this.setAccountName('');
+      this.clearLocalStorage();
+      if (this.$route.path !== '/') {
+        await this.$router.push({ path: '/' });
+      }
+    },
+    clearLocalStorage(): void {
+      localStorage.removeItem('autoLogin');
+      localStorage.removeItem('account');
+      localStorage.removeItem('anchor-link--list');
+      localStorage.removeItem('autoLogin');
+    }
+  }
 });
 </script>
 <template lang="pug">
-q-btn-dropdown.connect-button( color='primary' :label='account')
+q-btn-dropdown.connect-button( color='primary' :label='account' :content-style="{ backgroundColor: '#172c6c' }")
+  q-list
+    q-item(v-for='account in accounts' :label='account')
+  .buttons-container
+    q-btn.account-button(@click='attachAccount' color='primary' label='Attach an account')
+    q-btn.account-button(@click='onLogout' color='primary' :label='disconnectLabel')
 WalletModal(:showModal='false')
 </template>
-<style lang="sass">
+<style lang="sass" scoped>
+.q-menu
+  background-color: blue
+  min-width: unset
+.q-list
+  width: 12rem
+.account-button
+  width: calc(100% - 30px)
+  margin: 0 15px 15px 15px
 .connect-button
   text-transform: lowercase
 </style>
