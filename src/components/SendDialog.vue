305366e2f96bc7444af8358d3e9d8eac0fdb2493
Don Peat <don@caleos.io>
2022-03-23 00:55:31 -0500

    fix: equality for find system token

--- src/components/SendDialog.vue
+++ src/components/SendDialog.vue
@@ -93,7 +93,7 @@ export default defineComponent({
   mounted() {
     if (this.availableTokens.length > 0) {
       this.sendToken = this.availableTokens.find((token) => {
-        return (token.symbol = this.sendToken.symbol);
+        return token.symbol === this.sendToken.symbol;
       });
     }
   },
