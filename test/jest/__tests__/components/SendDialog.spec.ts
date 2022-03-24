6bde102c5790b47a29e53a183b11f072f42f3e09
Don Peat <don@caleos.io>
2022-03-24 12:52:51 -0500

    test: init sendDialog tests, @show

---
+++ test/jest/__tests__/components/SendDialog.spec.ts
@@ -0,0 +1,78 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  describe,
+  expect,
+  it,
+  jest,
+  afterEach,
+  beforeEach
+} from '@jest/globals';
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
+import { shallowMount } from '@vue/test-utils';
+import SendDialog from 'src/components/SendDialog.vue';
+
+installQuasarPlugin();
+
+describe('SendDialog', () => {
+  let wrapper: { vm: any };
+  beforeEach(() => {
+    wrapper = shallowMount(SendDialog, {
+      props: {
+        callback: jest.fn(),
+        openSendDialog: true,
+        availableTokens: []
+      },
+      mocks: {
+        $ual: {
+          getAuthenticators: jest.fn()
+        }
+      }
+    });
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  describe('@show', () => {
+    it('calls setDefaults', () => {
+      const methodSpy = jest.spyOn(SendDialog.methods as any, 'setDefaults');
+      wrapper = shallowMount(SendDialog, {
+        props: {
+          callback: jest.fn(),
+          openSendDialog: true,
+          availableTokens: []
+        },
+        mocks: {
+          $ual: {
+            getAuthenticators: jest.fn()
+          }
+        }
+      });
+      wrapper.vm.$nextTick(() => {
+        expect(methodSpy).toHaveBeenCalled();
+      });
+    });
+  });
+  // describe('methods', () => {
+  //   let wrapper: { vm: any };
+  //   beforeEach(() => {
+  //     wrapper = shallowMount(SendDialog, {
+  //       mocks: {
+  //         $ual: {
+  //           getAuthenticators: jest.fn()
+  //         }
+  //       }
+  //     });
+  //   });
+  //   afterEach(() => {
+  //     jest.clearAllMocks();
+  //   });
+  //   describe('sendTransaction', () => {
+  //     it('calls getTransactions', () => {
+  //       expect(wrapper.vm.$api.getTransactions).toHaveBeenCalled();
+  //     });
+  //   });
+  // });
+});
