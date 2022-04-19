fdd290e3106a70bec96caa9db4a5c6a8429eaef6
Wikus <wikus.kriek.2w@gmail.com>
2022-04-19 16:34:10 +0200

    fix: Typo

--- src/components/Transaction/TransactionCard.vue
+++ src/components/Transaction/TransactionCard.vue
@@ -113,7 +113,7 @@ export default defineComponent({
         q-card-section
           .row
             .col-xs-12.col-sm-6
-              .text-body1.text-weight-medium.text-uppercase Actions?Traces
+              .text-body1.text-weight-medium.text-uppercase Actions/Traces
             .col-xs-12.col-sm-6.text-right.text-bold {{actionNum+'/'+actionNum}}
 
 </template>
