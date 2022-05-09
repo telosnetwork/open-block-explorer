cf8192256c0bc84fb9d91927698d8a019b27e8db
Wikus <wikus.kriek.2w@gmail.com>
2022-05-09 16:55:01 +0200

    fix: Traces view improvement

---
+++ src/components/Transaction/AccountFormat.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent, toRef } from 'vue';
+
+export default defineComponent({
+  name: 'TransactionsTable',
+  props: {
+    account: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const type = toRef(props, 'type');
+    const account = toRef(props, 'account');
+
+    return {
+      accType: type,
+      accAccount: account
+    };
+  }
+});
+</script>
+
+<template lang="pug">
+a(:href=" '/' + type + '/' + accAccount" class="hover-dec") {{type === 'transaction' ? accAccount.slice(0, 8) : accAccount}}
+</template>
+
+<style lang="sass" scoped>
+.hover-dec
+  text-decoration: none
+  &:hover
+    text-decoration: underline
+</style>
