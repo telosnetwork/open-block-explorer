5d7fb21914eff9f0c84b180d210b3869d7ee884c
Elton Pongilo <elton@detroitledger.tech>
2022-06-02 17:36:17 -0300

    feat: create msig proposal form

--- src/types/index.ts
+++ src/types/index.ts
@@ -17,7 +17,8 @@ export {
   Refund,
   Userres,
   Block,
-  Get_actions
+  Get_actions,
+  Error
 } from './Actions';
 export { TransactionTableRow } from './TransactionTableRow';
 export { PaginationSettings } from './PaginationSettings';
@@ -29,5 +30,11 @@ export { RexbalRows, Rexbal, ProducerRows } from './TableRows';
 export { BP } from './BP';
 export { ChainInfo } from './ChainInfo';
 export { ProducerSchedule } from './ProducerSchedule';
-export { GetProposalsProps, GetProposals } from './Proposal';
+export {
+  GetProposalsProps,
+  GetProposals,
+  ProposalTableRow,
+  ProposalForm,
+  RequestedApprovals
+} from './Proposal';
 export { GetProducers } from './Producers';
