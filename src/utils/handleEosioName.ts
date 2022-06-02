04a415590e94b103fdd2c04fba6250b5e5307236
Elton Pongilo <elton@detroitledger.tech>
2022-06-02 17:32:34 -0300

    chore: create ramdom eosio name

---
+++ src/utils/handleEosioName.ts
@@ -0,0 +1,12 @@
+export function randomEosioName(length = 12): string {
+  let result = '';
+  const validCharacters = '12345abcdefghijklmnopqrstuvxyz';
+
+  for (let i = 0; i < length; i++) {
+    result += validCharacters.charAt(
+      Math.floor(Math.random() * validCharacters.length)
+    );
+  }
+
+  return result;
+}
