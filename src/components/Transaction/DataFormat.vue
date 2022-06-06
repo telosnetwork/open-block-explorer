af63f7cd9bfd2be4dc9dad89e862617488ea7095
Wikus <wikus.kriek.2w@gmail.com>
2022-06-06 15:53:22 +0200

    chore: changed html injection to components

--- src/components/Transaction/DataFormat.vue
+++ src/components/Transaction/DataFormat.vue
@@ -27,24 +27,29 @@ export default defineComponent({
       }
     });
 
-    function formatGeneralData(data: any): any {
-      var dict = []
+    function formatGeneralData(data: any): any[] {
+      console.log('data: ', data)
+      var dict: any[] = [];
       for (let key in data) {
         if (data[key] instanceof Object) {
           if (Array.isArray(data[key])) {
-            var keyValArray = [];
+            var keyValArray: any[] = [];
             for (let i = 0; i < data[key].length; i++) {
-              keyValArray.push(formatGeneralData(data[key][i]));
+              keyValArray = keyValArray.concat(formatGeneralData(data[key][i]));
             }
-            return keyValArray;
+            dict = dict.concat(keyValArray);
           } else {
-            var keyValArray = [];
+            let keyValArray = [];
+            console.log('execute');
+            console.log(formatGeneralData(data[key]));
             keyValArray.push(formatGeneralData(data[key]));
+            dict = dict.concat(keyValArray);
           }
         } else {
           dict.push({key, value:data[key]});
         }
       }
+      console.log('dict:',dict);
       return dict;
     }
 
