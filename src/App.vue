90dd62674e80de01cfbfa905fefb747ab7c50c95
Don Peat <don@caleos.io>
2022-08-05 15:43:40 -0500

    feat: change favicon on nav

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,27 @@ import { setCssVar } from 'quasar';
 import ConfigManager from 'src/config/ConfigManager';
 import { themeProps } from './types/Theme';
 
-const theme = ConfigManager.get().getCurrentChain().getTheme();
+const chain = ConfigManager.get().getCurrentChain();
+const chainName = chain.getName();
+const theme = chain.getTheme();
 
 setTheme();
+setMetaData();
 
 function setTheme(): void {
   for (var themeVar of themeProps) {
     if (theme[themeVar]) setCssVar(themeVar, theme[themeVar]);
   }
+}
+
+function setMetaData(): void {
+  var link = document.querySelector("link[rel~='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    (link as HTMLLinkElement).rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+  (link as HTMLLinkElement).href = `chains/${chainName}/favicon.png`;
 }
 
 export default {
