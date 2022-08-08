6db06191ee5073709a2098e5c64ace5c9adf8e74
Don Peat <don@caleos.io>
2022-08-08 15:06:21 -0500

    fix: move chain assignment above sidebar conditional to display app when sidebar is disabled

--- src/config/ConfigManager.ts
+++ src/config/ConfigManager.ts
@@ -20,10 +20,9 @@ export default class ConfigManager {
   private init(): void {
     const showSidebar = process.env.SHOW_SIDEBAR;
     const configuredChain = process.env.CHAIN_NAME;
+    this.testnets = chainsConfig.testnets;
+    this.mainnets = chainsConfig.mainnets;
     if (showSidebar) {
-      this.testnets = chainsConfig.testnets;
-      this.mainnets = chainsConfig.mainnets;
-
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       const userConfiguredChain = this.getSelectedChain();
 
@@ -70,6 +69,7 @@ export default class ConfigManager {
   }
 
   private findChain(chainName: string) {
+    debugger;
     const fromMainnet = this.mainnets.find((c) => c.getName() === chainName);
     if (fromMainnet) return fromMainnet;
 
