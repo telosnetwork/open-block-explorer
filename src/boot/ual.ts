ea517ff488b49a64e26e8358cb32a0c54386261f
Wikus <wikus.kriek.2w@gmail.com>
2022-08-08 17:24:01 +0200

    fix: cleos dialogs

--- src/boot/ual.ts
+++ src/boot/ual.ts
@@ -2,6 +2,7 @@ import { boot } from 'quasar/wrappers';
 import { Authenticator, UAL, User } from 'universal-authenticator-library';
 import { Anchor } from 'ual-anchor';
 import { CleosAuthenticator } from '@telosnetwork/ual-cleos';
+import { Dialog, Notify, copyToClipboard } from 'quasar';
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
@@ -23,20 +24,72 @@ export default boot(({ app }) => {
     ]
   };
 
-  function loginHandler() {
-    let accountName = '';
+  async function loginHandler() {
+    let accountName = 'eosio';
+    let permission = 'active';
     if (localStorage.getItem('autoLogin') === 'cleos') {
       accountName = localStorage.getItem('account');
     } else {
-      accountName = prompt('Input you account name', '');
+      //accountName = prompt('Input you account name', '');
+      await new Promise((resolve) => {
+        Dialog.create({
+          color: 'primary',
+          title: 'Connect to cleos',
+          message: 'Account name',
+          prompt: {
+            model: '',
+            type: 'text' // optional
+          },
+          cancel: true,
+          persistent: true
+        })
+          .onOk((data: string) => {
+            accountName = data != '' ? data : 'eosio';
+          })
+          .onCancel(() => {
+            //console.log('>>>> Cancel');
+            throw 'Canceled!';
+          })
+          .onDismiss(() => {
+            resolve(true);
+          });
+      });
+      await new Promise((resolve) => {
+        Dialog.create({
+          color: 'primary',
+          title: 'Connect to cleos',
+          message: 'Account permission',
+          options: {
+            type: 'radio',
+            model: [],
+            // inline: true
+            items: [
+              { label: 'Active', value: 'active' },
+              { label: 'Owner', value: 'owner' }
+            ]
+          },
+          cancel: true,
+          persistent: true
+        })
+          .onOk((data: string) => {
+            permission = data;
+          })
+          .onCancel(() => {
+            //console.log('>>>> Cancel');
+            throw 'Canceled!';
+          })
+          .onDismiss(() => {
+            resolve(true);
+          });
+      });
     }
     return {
       accountName,
-      permission: 'active'
+      permission
     };
   }
 
-  function signHandler(trx: any) {
+  async function signHandler(trx: string) {
     // TODO: dialog prompt with cleos command for this transaction
     /*
     cleos -u https://telos.greymass.com push transaction '{
@@ -56,10 +109,46 @@ export default boot(({ app }) => {
       null,
       4
     );
-
-    alert(
-      `cleos -u https://${process.env.NETWORK_HOST} push transaction '${trxJSON}'`
-    );
+    await new Promise((resolve) => {
+      Dialog.create({
+        color: 'primary',
+        message: `<pre>cleos -u https://${process.env.NETWORK_HOST} push transaction '${trxJSON}'</pre>`,
+        html: true,
+        cancel: true,
+        fullWidth: true,
+        ok: {
+          label: 'Copy'
+        }
+      })
+        .onOk(() => {
+          // console.log('OK')
+          copyToClipboard(
+            `cleos -u https://${process.env.NETWORK_HOST} push transaction '${trxJSON}`
+          )
+            .then((): void => {
+              Notify.create({
+                color: 'green-4',
+                textColor: 'white',
+                message: 'Copied to clipboard',
+                timeout: 1000
+              });
+            })
+            .catch(() => {
+              Notify.create({
+                color: 'red-8',
+                textColor: 'white',
+                message: 'Could not copy',
+                timeout: 1000
+              });
+            });
+        })
+        .onCancel(() => {
+          //console.log('>>>> Cancel');
+        })
+        .onDismiss(() => {
+          resolve(true);
+        });
+    });
   }
 
   const authenticators: Authenticator[] = [
