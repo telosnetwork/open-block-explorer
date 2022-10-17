c4fad0f37dd74ee49bd6351f15b7df069c42d828
Wikus <wikus.kriek.2w@gmail.com>
2022-10-17 15:26:07 +0200

    feat: withdraw button for rexfund

--- src/store/account/state.ts
+++ src/store/account/state.ts
@@ -25,6 +25,7 @@ export interface AccountStateInterface {
   maturedRex: string;
   savingsRex: string;
   tlosRexRatio: number;
+  rexfund: number;
   authenticatorName: string;
 }
 
@@ -52,11 +53,12 @@ export function state(): AccountStateInterface {
     rexbal: {} as Rexbal,
     vote: [],
     abi: { abi: null } as ABI,
-    coreRexBalance: `0 ${chain.getSymbol()}`,
-    maturingRex: `0 ${chain.getSymbol()}`,
-    maturedRex: `0 ${chain.getSymbol()}`,
-    savingsRex: `0 ${chain.getSymbol()}`,
+    coreRexBalance: `0.0000 ${chain.getSymbol()}`,
+    maturingRex: `0.0000 ${chain.getSymbol()}`,
+    maturedRex: `0.0000 ${chain.getSymbol()}`,
+    savingsRex: `0.0000 ${chain.getSymbol()}`,
     tlosRexRatio: 1,
+    rexfund: 0,
     authenticatorName: null
   };
 }
