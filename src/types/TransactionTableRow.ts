3d444f8e630c33df2019627a4df9bf07a6402f02
Wikus <wikus.kriek.2w@gmail.com>
2022-11-22 16:57:14 +0200

    fix: show multiple actions under transaction ID

--- src/types/TransactionTableRow.ts
+++ src/types/TransactionTableRow.ts
@@ -1,8 +1,17 @@
 import { Action } from 'src/types';
+
+export interface TransactionTableActionRow {
+  name: string;
+  transaction: { id: string; type: string };
+  timestamp: string;
+  action: Action;
+  data: { data: unknown | Action['data']; name: string };
+}
 export interface TransactionTableRow {
   name: string;
   transaction: { id: string; type: string };
   timestamp: string;
   action: Action;
-  data: { data: unknown; name: string };
+  data: { data: unknown | Action['data']; name: string };
+  actions: TransactionTableActionRow[];
 }
