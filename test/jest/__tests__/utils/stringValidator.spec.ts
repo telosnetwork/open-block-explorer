fdae86d5e5d1fa67ae6a66cc887f7633dacb12bc
Don Peat <don@caleos.io>
2023-02-16 12:41:03 -0600

    test: isValidTransactionHex

--- test/jest/__tests__/utils/stringValidator.spec.ts
+++ test/jest/__tests__/utils/stringValidator.spec.ts
@@ -1,12 +1,8 @@
+import { describe, expect, it } from '@jest/globals';
 import {
-  describe,
-  expect,
-  it //,
-  // jest,
-  // afterEach,
-  // beforeEach
-} from '@jest/globals';
-import { isValidAccount } from 'src/utils/stringValidator';
+  isValidAccount,
+  isValidTransactionHex
+} from 'src/utils/stringValidator';
 
 describe('stringValidator utility functions', () => {
   describe('isValidAccount', () => {
@@ -25,6 +21,26 @@ describe('stringValidator utility functions', () => {
     it('returns false for accounts names exceeding 13 characters', () => {
       const invalidAntelopeAccount = 'abcdefghijklmn';
       expect(isValidAccount(invalidAntelopeAccount)).toBe(false);
+    });
+  });
+
+  describe('isValidTransactionHex', () => {
+    it('returns true if value is 64 characters in length containing valid hex (0-9, A-F, a-f)', () => {
+      const validTransactionHex =
+        '0000000000000000000000000000000000000000000123456789abcdefABCDEF';
+      expect(isValidTransactionHex(validTransactionHex)).toBe(true);
+    });
+    it('returns false if hex does not contain 64 characters', () => {
+      const invalidTransactionHex = 'FFFFFF';
+      expect(isValidTransactionHex(invalidTransactionHex)).toBe(false);
+    });
+    it('returns false if string does not contain valid uppercase characters', () => {
+      const invalidTransactionHex = 'ABCDEFG';
+      expect(isValidTransactionHex(invalidTransactionHex)).toBe(false);
+    });
+    it('returns false if string does not contain valid lowercase characters', () => {
+      const invalidTransactionHex = 'abcdefg';
+      expect(isValidTransactionHex(invalidTransactionHex)).toBe(false);
     });
   });
 });
