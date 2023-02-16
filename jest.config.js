acfb43e468ec23dc01b9ec22fb17640fcc4c70af
Don Peat <don@caleos.io>
2023-02-16 12:45:14 -0600

    test: bump coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -33,10 +33,10 @@ module.exports = {
   //TODO increase thresholds as coverage increases as testing will be enforced
   coverageThreshold: {
     global: {
-      statements: 5,
-      branches: 2,
-      functions: 2.5,
-      lines: 5
+      statements: 13,
+      branches: 4,
+      functions: 4.5,
+      lines: 14
     },
     './src/components/': {
       statements: 0,
