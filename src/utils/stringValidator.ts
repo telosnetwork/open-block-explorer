6ab91e762f645d28668afa3a5218c4c5db08ff26
Don Peat <don@caleos.io>
2023-02-16 12:14:19 -0600

    fix: typo in account regexp

--- src/utils/stringValidator.ts
+++ src/utils/stringValidator.ts
@@ -1,7 +1,7 @@
 /* regex to validate EOSIO account name convention see: https://regex101.com/r/d8uKrG/1 */
 
 export function isValidAccount(account: string): boolean {
-  const regEx = /(^[a-z1-5.]{1,11}[a-z1-5]$)|(^[a-z1-5.]{12}[a-j1-5]$)/;
+  const regEx = /(^[a-z1-5.]{1,11}[a-z1-5]$)|(^[a-z1-5.]{12}[a-z1-5]$)/;
   return regEx.exec(account) != null;
 }
 
