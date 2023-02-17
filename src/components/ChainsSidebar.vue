9901a843fe6c296e38c15c8d2294ce56ca5105c5
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-02-17 16:39:52 -0500

    feat: misc updates

--- src/components/ChainsSidebar.vue
+++ src/components/ChainsSidebar.vue
@@ -1,82 +1,94 @@
-<template lang="pug">
-q-drawer(
-  show-if-above
-  :mini="miniState"
-  @mouseover="miniState = false"
-  @mouseout="miniState = true"
-  mini-to-overlay
-  :width="175"
-  :breakpoint="500"
-  bordered
-)
-  q-scroll-area.fit
-    q-list
-      template( v-for="(chain, index) in getMainnets()" :key="index" )
-        q-item(clickable v-ripple @click="chainSelected(chain)" :class="{ selected: isSelected(chain) }")
-          q-avatar
-            img( :src="chain.getSmallLogoPath()" ).sidebar-logo
-          q-item-section.margin-left
-            div {{ chain.getDisplay() }}
-      q-separator.separator
-      template( v-for="(chain, index) in getTestnets()" :key="index" )
-        q-item(clickable v-ripple @click="chainSelected(chain)" :class="{ selected: isSelected(chain) }")
-          q-avatar
-            img( :src="chain.getSmallLogoPath()" ).sidebar-logo
-          q-item-section.margin-left
-            div {{ chain.getDisplay() }}
-</template>
-
 <script lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import ConfigManager from 'src/config/ConfigManager';
 import { Chain } from 'src/types/Chain';
 
 const configMgr = ConfigManager.get();
 
 export default {
-  name: 'ChainsSidebar',
-  setup() {
-    return {
-      miniState: ref(true),
-      getAllChains: () => {
-        return configMgr.getAllChains();
-      },
-      getMainnets: () => {
-        return configMgr.getMainnets();
-      },
-      getTestnets: () => {
-        return configMgr.getTestnets();
-      },
-      chainSelected(chain: Chain) {
-        if (this.isSelected(chain)) {
-          return;
+    name: 'ChainsSidebar',
+    setup() {
+        function sortChainsUsingName(chains: Chain[]): Chain[] {
+            return chains.sort(
+                (chain1, chain2) => chain1.getName().localeCompare(chain2.getName()),
+            );
         }
-        // TODO: maybe we can reload vue store and boot files instead of full reload?
-        localStorage.setItem(
-          ConfigManager.CHAIN_LOCAL_STORAGE,
-          chain.getName()
-        );
-        location.reload();
-      },
-      isSelected(chain: Chain): boolean {
-        return (
-          localStorage.getItem(ConfigManager.CHAIN_LOCAL_STORAGE) ===
-          chain.getName()
-        );
-      }
-    };
-  }
+
+        const mainnets = computed(() => sortChainsUsingName(configMgr.getMainnets()));
+        const testnets = computed(() => sortChainsUsingName(configMgr.getTestnets()));
+
+        return {
+            miniState: ref(true),
+            mainnets,
+            testnets,
+            chainSelected(chain: Chain) {
+                if (this.isSelected(chain)) {
+                    return;
+                }
+                // TODO: maybe we can reload vue store and boot files instead of full reload?
+                localStorage.setItem(
+                    ConfigManager.CHAIN_LOCAL_STORAGE,
+                    chain.getName(),
+                );
+                location.reload();
+            },
+            isSelected(chain: Chain): boolean {
+                return localStorage.getItem(ConfigManager.CHAIN_LOCAL_STORAGE) === chain.getName();
+            },
+        };
+    },
 };
 </script>
 
+<template lang="pug">
+q-drawer(
+    show-if-above
+    :mini="miniState"
+    @mouseover="miniState = false"
+    @mouseout="miniState = true"
+    mini-to-overlay
+    :width="175"
+    :breakpoint="500"
+    bordered
+)
+    q-scroll-area.fit
+        q-list
+            q-item(
+                v-for="(chain, index) in mainnets"
+                :key="`mainnet-${index}`"
+                :class="{ selected: isSelected(chain) }"
+                clickable
+                v-ripple
+                @click="chainSelected(chain)"
+            )
+                img( :src="chain.getSmallLogoPath()" ).sidebar-logo
+                q-item-section
+                    .q-pl-md {{ chain.getDisplay() }}
+
+            q-separator.separator
+
+            q-item(
+                v-for="(chain, index) in testnets"
+                :key="`testnet-${index}`"
+                :class="{ selected: isSelected(chain) }"
+                clickable
+                v-ripple
+                @click="chainSelected(chain)"
+            )
+                .testnet-logo-container
+                    img( :src="chain.getSmallLogoPath()" ).sidebar-logo.sidebar-logo--testnet
+                    .testnet-text Testnet
+                q-item-section
+                    .q-pl-md {{ chain.getDisplay() }}
+</template>
+
 <style lang="sass" scoped>
-.margin-left
-  margin-left: .5rem
 .q-item
   &:hover, &.selected
     background-color: var(--q-color-sidebar-selected)
   padding-left: 4px
   padding-top: 4px
+
 .separator
   margin-top: .5rem
   margin-bottom: .5rem
@@ -85,7 +97,33 @@ export default {
   min-width: 0
   width: calc(100% - 10px)
   background: var(--q-color-sidebar-selected)
+
 .sidebar-logo
-  height: 35px !important
-  width: 35px !important
+  height: auto
+  width: auto
+  max-height: 48px
+  max-width: 48px
+  object-fit: contain
+
+.testnet-logo-container
+    position: relative
+    height: 48px
+    width: 48px
+
+.testnet-text, .sidebar-logo--testnet
+    position: absolute
+    margin: auto
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+
+.testnet-text
+    color: white
+    font-size: 12px
+    width: min-content
+    height: min-content
+    padding: 0 2px
+    border-radius: 2px
+    background-color: rgba(black, 0.6)
 </style>
