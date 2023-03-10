2e6e94b48199dc609151a0854f0e26931cf91926
Aaron Cox <aaron@greymass.com>
2023-03-10 11:16:27 -0500

    Removing two debug wallets

--- src/boot/wharf.ts
+++ src/boot/wharf.ts
@@ -1,11 +1,9 @@
 import { boot } from 'quasar/wrappers';
 import { Chain } from 'src/types/Chain';
 import { getChain } from 'src/config/ConfigManager';
-import { BrowserLocalStorage, Checksum256, PermissionLevel, Session, SessionKit } from '@wharfkit/session';
+import { BrowserLocalStorage, Session, SessionKit } from '@wharfkit/session';
 import { WalletPluginAnchor } from '@wharfkit/wallet-plugin-anchor';
 import { WalletPluginCleos } from '@wharfkit/wallet-plugin-cleos';
-import { WalletPluginMock } from '@wharfkit/wallet-plugin-mock';
-import { WalletPluginPrivateKey } from '@wharfkit/wallet-plugin-privatekey';
 import WebUIRenderer from '@wharfkit/web-ui-renderer';
 
 const chain: Chain = getChain();
@@ -32,15 +30,6 @@ export const kit = new SessionKit({
     walletPlugins: [
         new WalletPluginAnchor(),
         new WalletPluginCleos(),
-        new WalletPluginMock({
-            loginResponse: {
-                chain: Checksum256.from(chain.getChainId()),
-                permissionLevel: PermissionLevel.from('eosio@active'),
-            },
-        }),
-        new WalletPluginPrivateKey(
-            '5Jtoxgny5tT7NiNFp1MLogviuPJ9NniWjnU4wKzaX4t7pL4kJ8s',
-        ),
     ],
 });
 
