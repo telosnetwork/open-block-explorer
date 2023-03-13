f01436bbd35f42fc18d0e27658cfc4dc02f30ccf
Viterbo <viter.rod@gmail.com>
2023-03-13 17:21:38 -0300

    remove some warnings

--- src/components/SendDialog.vue
+++ src/components/SendDialog.vue
@@ -141,7 +141,6 @@ export default defineComponent({
     watch: {
         availableTokens: {
             handler() {
-                console.log('availableTokens changed', this.availableTokens);
                 if (this.availableTokens.length > 0) {
                     this.sendToken = this.availableTokens.find(token => (
                         token.symbol === this.sendToken.symbol &&
