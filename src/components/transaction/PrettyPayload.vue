f3af0cc7ff551821efd84f81f76b90b1a0772acb
Karyne Mayer <karyne@detroitledger.tech>
2023-06-26 10:36:18 -0700

    Indentation fix

--- src/components/transaction/PrettyPayload.vue
+++ src/components/transaction/PrettyPayload.vue
@@ -3,12 +3,12 @@ import { defineComponent, computed, toRefs } from 'vue';
 
 export default defineComponent({
     name: 'PrettyPayload',
-    props: { depth: { type: Number, required: true },
+    props: {
+        depth: { type: Number, required: true },
         payload: { type: Object, required: true },
     },
     setup(props) {
         const { depth, payload } = toRefs(props);
-        const indent = { transform: `translate(${depth.value}px)` };
         const type = payload.value === null ? 'null' : typeof payload.value;
 
         const list = computed(() => {
@@ -18,10 +18,15 @@ export default defineComponent({
             return undefined;
         });
 
+        const classObject = computed(() => ({
+            'payload': true,
+            'payload__indent': depth.value > 0,
+        }));
+
         return {
-            indent,
             type,
             list,
+            classObject,
         };
     },
 
@@ -29,12 +34,20 @@ export default defineComponent({
 </script>
 
 <template>
-<div v-for="key, index in list" :key="index" class="payload">
+<div
+    v-for="key, index in list"
+    :key="index"
+    :class="classObject"
+>
     <template v-if="Array.isArray(payload[key])">
         <span class="text-bold">{{ key }}:</span>
         <span v-if="payload[key].length === 0">{{` [ ]`}}</span>
-        <ul v-else>
-            <li v-for="(item, index) in payload[key]" :key="index">
+        <ul v-else class="payload__list">
+            <li
+                v-for="(item, index) in payload[key]"
+                :key="index"
+                class="payload__list-item"
+            >
                 <PrettyPayload
                     :depth="depth + 1"
                     :payload="item"
@@ -43,13 +56,11 @@ export default defineComponent({
         </ul>
     </template>
     <template v-else-if="typeof payload[key] === 'object'">
-        <span class="text-bold">{{ key +':'}}</span>
-        <div :style="indent">
-            <PrettyPayload
-                :depth="depth + 1"
-                :payload="payload[key]"
-            />
-        </div>
+        <div class="text-bold">{{ key +':'}}</div>
+        <PrettyPayload
+            :depth="depth + 1"
+            :payload="payload[key]"
+        />
     </template>
     <template v-else>
         <span class="text-bold">{{ key }}:</span> {{ payload[key] }}
@@ -60,4 +71,16 @@ export default defineComponent({
 <style lang="sass" scoped>
 .payload
     max-width: 100%
+    white-space: normal
+    &__indent
+        margin-left: 1rem
+    &__list
+        margin: 0
+        padding: 0
+    &__list-item
+        list-style-position: inside
+        list-style: none
+        margin: 0
+        padding: 0
+
 </style>
