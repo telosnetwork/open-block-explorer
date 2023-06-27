21e699fda09932b586d5c9f7a297e7541156ad5e
Eduardo Jaeger <eduardo@eosdetroit.io>
2023-06-27 09:11:46 -0300

    Including changes for theme

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,13 +2,58 @@
 import AppFooter from 'components/Footer.vue';
 import AppHeader from 'components/Header.vue';
 
-export default {
+import { setCssVar } from 'quasar';
+import ConfigManager from 'src/config/ConfigManager';
+import { themeProps } from 'src/types/Theme';
+import { DEFAULT_THEME } from 'src/config/BaseChain';
+import { defineComponent, onMounted, watch } from 'vue';
+import { useRouteDataNetwork } from 'src/router';
+
+export default defineComponent({
     name: 'MainLayout',
     components: {
         AppHeader,
         AppFooter,
     },
-};
+    setup() {
+        const networkChain = useRouteDataNetwork();
+
+        function setTheme(): void {
+            const theme = ConfigManager.get().getCurrentChain().getTheme();
+            for (let themeVar of themeProps) {
+                if (theme[themeVar]) {
+                    setCssVar(themeVar, theme[themeVar]);
+                } else {
+                    setCssVar(themeVar, DEFAULT_THEME[themeVar]);
+                }
+            }
+        }
+
+        function setMetaData(): void {
+            const chainName = ConfigManager.get().getCurrentChain().getName();
+            console.log(chainName);
+            let link = document.querySelector('link[rel~="icon"]');
+            (link as HTMLLinkElement).href = `chains/${chainName}/favicon.png`;
+
+            document.title = chainName;
+        }
+
+        watch(networkChain, () => {
+            console.log('updating chain');
+            setTheme();
+            setMetaData();
+        });
+
+        onMounted(() => {
+            setTheme();
+            setMetaData();
+        });
+
+        return {
+            networkChain,
+        };
+    },
+});
 </script>
 
 <template>
