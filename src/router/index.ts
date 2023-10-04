9f4a0b91689d9c5e8a5eda8293f2ef1365a67107
Eduardo Jaeger <eduardo@detroitledger.tech>
2023-10-04 14:44:37 -0300

    Fixing landing page on single chain mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -42,6 +42,14 @@ export default route<StateInterface>(function (/* { store, ssrContext } */) {
         const preferredChainName = configMgr.getPreferredChain();
 
         if (to.path === '/') { // if attempting to go to home page
+            if (process.env.SHOW_MULTICHAIN_SELECTOR !== 'true') {
+                return({
+                    path: 'network',
+                    query: {
+                        network: process.env.CHAIN_NAME,
+                    },
+                });
+            }
             if (to.query.network) { // if there is network param, proceed to network with the param
                 return ({
                     path: 'network',
