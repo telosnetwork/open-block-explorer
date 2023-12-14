020a0c49486b311b1cd3638cc66b89cde2b812d5
donnyquixotic <don@caleos.io>
2023-12-14 13:34:33 -0600

    fix: adjust debounce for header

--- src/components/HeaderSearch.vue
+++ src/components/HeaderSearch.vue
@@ -37,7 +37,7 @@ export default defineComponent({
             isLoading.value = false;
         };
 
-        const onInput = debounce(fetchData, 100);
+        const onInput = debounce(fetchData, 200);
 
         watch(inputValue, onInput);
 
